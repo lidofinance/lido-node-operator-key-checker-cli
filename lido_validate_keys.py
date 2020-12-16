c21ada5aa217b9ec6d5791b65f6da735151f2cb7
v <vladimirfol@gmail.com>
2020-12-16 12:34:56 +0300

    - refactoring - comments and todos added

--- lido_validate_keys.py
+++ lido_validate_keys.py
@@ -1,7 +1,7 @@
 import json
 
 import click
-
+from py_ecc.bls import G2ProofOfPossession as bls
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
 
@@ -11,7 +11,6 @@ from eth2deposit.utils.ssz import (
     compute_signing_root,
     DepositMessage,
 )
-from py_ecc.bls import G2ProofOfPossession as bls
 
 
 @click.group()
@@ -54,10 +53,10 @@ def cli(ctx, rpc_url, network, lido_address, nos_registry_address, lido_abi, nos
 
     # Load contracts from network
 
-    lido_abi = list(json.load(open(lido_abi)))
+    lido_abi = json.load(open(lido_abi))
     lido = w3.eth.contract(address=lido_address, abi=lido_abi)
 
-    operators_abi = list(json.load(open(nos_registry_abi)))
+    operators_abi = json.load(open(nos_registry_abi))
     operators = w3.eth.contract(address=nos_registry_address, abi=operators_abi)
 
     # Setting up required items for validation
@@ -102,6 +101,8 @@ def validate_network_keys(ctx):
 
                 # Checking if each key is a duplicate
 
+                # fixme: this works for O(N^2), but it's possible to do in O(N),
+                #  not a big deal if len(ops) < 100
                 duplicates = find_duplicates(ops, op, key)
                 ops[op_n]["keys"][key_n]["duplicate"] = bool(duplicates)
 
@@ -152,7 +153,7 @@ def validate_network_keys(ctx):
 @click.option(
     "--file",
     default="input.json",
-    help="JSON input file",
+    help="JSON input file with a node description",
 )
 @cli.command("validate_file_keys")
 @click.pass_context
@@ -165,43 +166,43 @@ def validate_file_keys(ctx, file):
     operators = ctx.obj["operators"]
 
     # Load and format JSON file
-    data = list(json.load(open(file)))
-    formatted = format_input_file(data)
+    node_description_raw = json.load(open(file))
+    node_description = format_input_node_description_file(node_description_raw)
 
     # Load network data
     ops = load_network_data(operators)
 
     # Search for duplicates
     duplicates = []
-    for i in formatted:
-        duplicate = find_duplicates(ops, "all", i)
+    for node_description_item in node_description:
+        duplicate = find_duplicates(ops, "all", node_description_item)
         if duplicate:
             duplicates.append(duplicate)
     if not duplicates:
         click.secho("No duplicate keys found", fg="green")
     else:
         click.secho("Duplicate keys found:", fg="red")
-        for duplicate_occurances in duplicates:
-            for i in duplicate_occurances:
+        for duplicate_occurrences in duplicates:
+            for duplicate_occurrence in duplicate_occurrences:
                 click.secho(
                     "Key %s is a duplicate of %s's key (OP Approved: %s, Key Used: %s)"
                     % (
-                        i["key"]["key"].hex(),
-                        i["op"]["name"],
-                        i["op"]["stakingLimit"] > 0,
-                        i["key"]["used"],
+                        duplicate_occurrence["key"]["key"].hex(),
+                        duplicate_occurrence["op"]["name"],
+                        duplicate_occurrence["op"]["stakingLimit"] > 0,
+                        duplicate_occurrence["key"]["used"],
                     ),
                     fg="red",
                 )
 
     # Check signatures
     invalid_signatures = []
-    with click.progressbar(formatted, label="Checking signatures", show_eta=False) as keys:
-        for i in keys:
-            valid = validate_key(i, withdrawal_credentials, domain)
+    with click.progressbar(node_description, label="Checking signatures", show_eta=False) as keys:
+        for node_description_item in keys:
+            valid = validate_key(node_description_item, withdrawal_credentials, domain)
 
             if not valid:
-                invalid_signatures.append(i["pubkey"])
+                invalid_signatures.append(node_description_item["pubkey"])
 
     if not invalid_signatures:
         click.secho("No invalid signatures found", fg="green")
@@ -214,13 +215,12 @@ def validate_file_keys(ctx, file):
 # Common helpers
 
 
-def format_input_file(items):
+def format_input_node_description_file(items):
     """Format input to the same data keys on the network"""
-
-    for n, i in enumerate(items):
-        items[n]["key"] = bytes.fromhex(i["pubkey"]) if i.get("pubkey", False) else i["key"]
-        items[n]["deposit_signature"] = (
-            bytes.fromhex(i["signature"]) if i.get("signature", False) else i["deposit_signature"]
+    for item in items:
+        item["key"] = bytes.fromhex(item["pubkey"]) if item.get("pubkey", False) else item["key"]
+        item["deposit_signature"] = (
+            bytes.fromhex(item["signature"]) if item.get("signature", False) else item["deposit_signature"]
         )
     return items
 
@@ -233,6 +233,8 @@ def load_network_data(operators):
     click.echo("There are %s operators on the network" % opcount)
 
     # Get each operator's data, add id
+    # fixme use named fields (specified contract's JSON)
+    #  to decrease the probability of mistake due to positional misplacing
     ops_raw = [[i] + operators.functions.getNodeOperator(i, True).call() for i in range(0, opcount)]
 
     # Assign keys for the data
@@ -281,39 +283,36 @@ def list_signing_keys(operators, op_id, key_count):
     return signing_keys_list
 
 
-def find_duplicates(ops, original_op, key):
+def find_duplicates(operators, original_op, key):
     """Compare every available key with each other to spot duplicates"""
-
     duplicates_found = []
-    for op in ops:
-        for second_key in op["keys"]:
+    for operator in operators:
+        for second_key in operator["keys"]:
             if key["key"] == second_key["key"]:
-                if original_op == "all":
-                    duplicates_found.append({"op": op, "key": second_key})
-                    continue
-                elif original_op["id"] == op["id"] and key["index"] == second_key["index"]:
-                    continue
-                else:
-                    duplicates_found.append({"op": op, "key": second_key})
+                if original_op == "all":  # TODO: unclear logic here, I don't understand
+                    duplicates_found.append({"op": operator, "key": second_key})
+                elif (original_op["id"], key["index"]) != (operator["id"], second_key["index"]):
+                    duplicates_found.append({"op": operator, "key": second_key})
     return duplicates_found
 
 
-def validate_key(i, withdrawal_credentials, domain):
+def validate_key(key, withdrawal_credentials, domain):
     """Run signature validation on a key"""
+    # TODO: it's better to add how it's validated, because it's unclear for me
 
-    pubkey = i["key"]
-    signature = i["deposit_signature"]
+    pubkey = key["key"]
+    signature = key["deposit_signature"]
 
+    REQUIRED_DEPOSIT_ETH = 32
     ETH2GWEI = 10 ** 9
-    amount = 32 * ETH2GWEI
+    amount = REQUIRED_DEPOSIT_ETH * ETH2GWEI
 
-    dm = DepositMessage(
+    deposit_message = DepositMessage(
         pubkey=pubkey,
         withdrawal_credentials=withdrawal_credentials,
         amount=amount,
     )
-
-    signing_root = compute_signing_root(dm, domain)
+    signing_root = compute_signing_root(deposit_message, domain)
     return bls.Verify(pubkey, signing_root, signature)
 
 
